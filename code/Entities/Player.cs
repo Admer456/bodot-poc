134f389dac667a4912baff0709e0253c5e72ed44
Admer <49600504+Admer456@users.noreply.github.com>
2022-12-18 22:47:58 +0100

    Player: separate movement code

--- code/Entities/Player.cs
+++ code/Entities/Player.cs
@@ -6,7 +6,7 @@ using Bodot.Utilities;
 
 namespace Bodot.Entities
 {
-	public class Player : Entity, IPlayerControllable
+	public partial class Player : Entity, IPlayerControllable
 	{
 		public const float PlayerHeight = 1.83f;
 		public const float EyeHeight = 1.74f;
@@ -36,71 +36,7 @@ namespace Bodot.Entities
 
 		public override void PhysicsUpdate( float delta )
 		{
-			const float Speed = 28.0f;
-			const float AirSpeed = Speed / 5.0f;
-			// 16 units converted to Godot units
-			const float Acceleration = 50.0f;
-			const float AccelerationMultiplier = 15.0f;
-			const float Friction = 4.0f;
-
-			float speedModifier = 1.0f;
-			if ( mLastCommands.ActionStates.HasFlag( ClientActions.Sprint ) )
-			{
-				speedModifier = 1.666f;
-			}
-
-			// On ground, apply friction'n'stuff
-			KinematicCollision3D collision = new();
-			bool onGround = mBody.TestMove( mBody.GlobalTransform, Vector3.Down * delta * 2.0f, collision );
-			float groundDot = onGround ? collision.GetNormal().Dot( Vector3.Up ) : 0.0f;
-
-			if ( onGround )
-			{
-				mBody.Velocity += mLastCommands.MovementDirection * Speed * speedModifier * delta;
-
-				// Cannot jump on slopes
-				if ( groundDot >= 0.7f )
-				{
-					mBody.Velocity *= 0.85f;
-	
-					if ( mLastCommands.MovementDirection.y > 0.0f )
-					{
-						mBody.Velocity += Vector3.Up * 160.0f * delta;
-					}
-				}
-			}
-			else
-			{
-				// We'd like the player to air accelerate only if moving perpendicular to the current velocity
-				// So the dot product of movedir and velocity needs to be transformed from 1,0,-1 into 0,1,0
-				Vector3 moveDirectionFlat = mLastCommands.MovementDirection * new Vector3( 1.0f, 0.0f, 1.0f );
-				Vector2 moveDirectionFlatNormalised = mLastCommands.MovementDirection.XZ().Normalized();
-				Vector2 bodyVelocityFlat = mBody.Velocity.XZ().Normalized();
-				float wishSpeed = Speed * moveDirectionFlatNormalised.Length();
-				float wishSpeedClamped = Mathf.Min( wishSpeed, 0.76f );
-
-				/*
-				float dotForward = moveDirectionFlatNormalised.Dot( bodyVelocityFlat );
-				float absDotSide = Mathf.Sqrt( 1.0f - Mathf.Abs( dotForward ) );
-
-				GD.Print( $"dotForward: {dotForward} (abs side: {absDotSide})" );
-
-				mBody.Velocity += mLastCommands.MovementDirection * new Vector3( 1.0f, 0.0f, 1.0f ) * AirSpeed * absDotSide * delta;
-				*/
-
-				float currentSpeed = moveDirectionFlatNormalised.Dot( bodyVelocityFlat );
-				float addSpeed = wishSpeedClamped - currentSpeed;
-				if ( addSpeed > 0.0f )
-				{
-					float accelerationSpeed = Mathf.Min( Acceleration * wishSpeed * Friction, addSpeed );
-					mBody.Velocity += accelerationSpeed * moveDirectionFlat * delta * AccelerationMultiplier;
-				}
-			}
-
-			mBody.MoveAndSlide();
-
-			mBody.Velocity += Vector3.Down * 9.81f * delta * (1.0f - Mathf.Abs( groundDot * groundDot ));
-			mBody.MoveAndSlide();
+			Move( delta );
 
 			mBody.GlobalRotation = new Vector3( 0.0f, mLastCommands.ViewAngles.y, 0.0f );
 		}
--- code/Utilities/GodotMathsExtensions.cs
+++ code/Utilities/GodotMathsExtensions.cs
@@ -80,6 +80,10 @@ namespace Bodot.Utilities
 		{
 			return new Vector2( vector.x, vector.y );
 		}
+		public static Vector3 XOZ( this Vector3 vector )
+		{
+			return new Vector3( vector.x, 0.0f, vector.z );
+		}
 		public static Vector2 XZ( this Vector3 vector )
 		{
 			return new Vector2( vector.x, vector.z );
