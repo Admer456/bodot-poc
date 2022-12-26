aa0277a201db6ed5970e01eb461038ea3d630aae
Admer <49600504+Admer456@users.noreply.github.com>
2022-12-26 21:57:23 +0100

    Add BodotAssemblyLoadContext

---
+++ code/BodotAssemblyLoadContext.cs
@@ -0,0 +1,22 @@
+﻿// SPDX-FileCopyrightText: 2022 Admer Šuko
+// SPDX-License-Identifier: MIT
+
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Bodot
+{
+	internal sealed class BodotAssemblyLoadContext : AssemblyLoadContext
+	{
+		protected override Assembly Load( AssemblyName assemblyName )
+		{
+			switch ( assemblyName.Name )
+			{
+				case "BodotPoc1": return Assembly.GetExecutingAssembly();
+				case "GodotSharp": return typeof( GD ).Assembly;
+			}
+
+			return null;
+		}
+	}
+}
